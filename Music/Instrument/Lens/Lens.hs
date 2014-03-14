09b634eeb844f87d1429830c567f74f773a4ff37
alan hawkins <hawk.alan@gmail.com>
2014-03-14 19:09:19 +1100

    fixed to work with latest chord library

--- Music/Instrument/Lens/Lens.hs
+++ Music/Instrument/Lens/Lens.hs
@@ -1,17 +1,41 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ImpredicativeTypes #-}
 
-import Music.Instrument.Chord
+module  Music.Instrument.Lens.Lens where
+
+import Music.Instrument.Chord hiding (scale)
 import Data.Default
-import Control.Lens
+import Control.Lens hiding (scale)
+
+
+data GuitarChordType = GuitarChordTypeChord Chord
+                     | GuitarChordTypeScale Scale
+  deriving Show
+
+class PositionPatternProgression a => GuitarChordTypeClass a where
+      toGuitarChordType :: a -> GuitarChordType
+      fromGuitarChordType :: GuitarChordType -> a
+
+instance GuitarChordTypeClass Chord where
+  toGuitarChordType c = GuitarChordTypeChord c
+  fromGuitarChordType (GuitarChordTypeChord c) = c
+
+
+instance GuitarChordTypeClass Scale where
+  toGuitarChordType c = GuitarChordTypeScale  c
+  fromGuitarChordType (GuitarChordTypeScale c) = c
 
 data GuitarChordOptions = GuitarChordOptions { 
       _annotationStyle :: ControlAnnotation 
      ,_firstTuningFirst :: Bool
      ,_vertical :: Bool
      ,_tuning :: [Note]
-     ,_chord :: Chord
+     ,_concept' :: GuitarChordType
      ,_maximumPatternHeight :: Int
+     ,_from :: Int
     }
     
 makeLenses ''GuitarChordOptions
@@ -21,10 +45,21 @@ instance Default GuitarChordOptions where
             _annotationStyle = AnnotateMarking
           , _firstTuningFirst = False
           , _vertical = True
-          , _maximumPatternHeight = 4
           , _tuning = standardTuning
-          , _chord = majorChord C
+          , _concept' = GuitarChordTypeChord (majorChord C)
+          , _maximumPatternHeight = 4
+          , _from = 0
         }
 
+concept :: (Functor f, GuitarChordTypeClass a) => (a -> f a) -> GuitarChordOptions -> f GuitarChordOptions
+concept = lens ( \x -> fromGuitarChordType (view concept' x)) (\a b -> set concept' (toGuitarChordType b) $ a )
+
+chord :: Functor f => (Chord -> f Chord) -> GuitarChordOptions -> f GuitarChordOptions
+chord = concept
+
+scale :: Functor f => (Scale-> f Scale) -> GuitarChordOptions -> f GuitarChordOptions
+scale = concept
+
 instance Show GuitarChordOptions where
-  show (GuitarChordOptions a f v t c m) = renderGuitarChords a f v t c m
+  show (GuitarChordOptions a f v t (GuitarChordTypeChord c) m fr) = renderGuitarChord a f v t c m fr
+  show (GuitarChordOptions a f v t (GuitarChordTypeScale s) m fr) = renderGuitarChord a f v t s m fr
